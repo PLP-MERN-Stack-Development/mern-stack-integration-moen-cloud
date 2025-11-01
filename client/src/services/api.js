d261319398bf86668eb734d640b000cd362fce8e
moen-cloud <moenmwangi@gmail.com>
2025-11-01 20:30:16 +0300

    Fixed backend issues

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://blogsphere-api-wry8.onrender.com',
 });
 
 API.interceptors.request.use((req) => {
